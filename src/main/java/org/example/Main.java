aebc052eab5e38059898a9eba23120f0a6669610
samue <samuel.jdev11@gmail.com>
2024-03-26 15:48:21 -0300

    Declarando e inicializando arrays

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] notas1 = new int[5];
+
+        int[] notas2;
+        notas2 = new int[]{8, 5, 4, 9, 10};
+
+        int[] notas3 = new int[]{8, 5, 4, 9, 10};
+
+        int[] notas4 = {8, 5, 4, 9, 10};
     }
 }
